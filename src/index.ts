4d30c1d9a2b2dc4f1dd45be2682eb2cfc856c980
keiya01 <keiya.s.0210@gmail.com>
2019-06-01 09:17:27 +0900

    Add scrape function to get uri

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello, World");
+import puppeteer from "puppeteer";
+
+const recieveRequest = (searchWrod: string) => {
+  return `https://www.google.co.jp/search?num=10&q=${searchWrod}`;
+}
+
+const scrape = async () => {
+  const uri = recieveRequest("typescript scraping");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(uri);
+
+  const links = await page.evaluate(`
+    function getUri() {
+      const elms = document.querySelector('#rso').firstChild.querySelectorAll('.r');
+      const hrefs = [];
+      for(let i = 0; i < elms.length; i++) {
+        const elm = elms[i];
+        hrefs.push(elm.firstChild.getAttribute("href"));
+      }
+      return hrefs;
+    }
+    getUri();
+  `);
+
+  console.log(links);
+
+  browser.close();
+
+  return;
+}
+
+scrape();
