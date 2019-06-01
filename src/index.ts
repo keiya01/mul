00cf6689e599cc8de7bcda0d5ce45030d1ec9a9b
keiya01 <keiya.s.0210@gmail.com>
2019-06-01 23:48:42 +0900

    Fix reset

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import puppeteer from "puppeteer";
 
-const recieveRequest = (searchWrod: string) => {
-  return `https://www.google.co.jp/search?num=10&q=${searchWrod}`;
+const newStdin = () => {
+  const stdin = process.stdin;
+  stdin.setEncoding("utf-8");
+  return stdin;
 }
 
-const scrape = async () => {
-  const uri = recieveRequest("typescript scraping");
+const scrape = async (searchWord: string) => {
+  const uri = `https://www.google.co.jp/search?num=5&q=${searchWord}`;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(uri);
 
   const links = await page.evaluate(`
     function getUri() {
-      const elms = document.querySelector('#rso').firstChild.querySelectorAll('.r');
+      const elms = document.querySelector('#rso').querySelectorAll('.r');
       const hrefs = [];
       for(let i = 0; i < elms.length; i++) {
         const elm = elms[i];
@@ -23,11 +25,42 @@ const scrape = async () => {
     getUri();
   `);
 
-  console.log(links);
-
   browser.close();
 
-  return;
+  return links;
 }
 
-scrape();
+const main = () => {
+  console.log("Search inputted word to find 5 urls of each words");
+
+  const stdin = newStdin();
+  
+  process.stdout.write("gsearch > ");
+  stdin.on("data", async (_data: string) => {
+    const data = _data.slice(0, _data.length - 1);
+
+    // Make user enter data with / break
+    const searchWords = data.split("/");
+
+    const asyncSearch = searchWords.map(async word => {
+      if(word === "") {
+        console.log("Can not empty");
+        process.stdout.write("gsearch > ");
+        return;
+      }
+      
+      if(word === "exit\n") {
+        console.log("done!\n");
+        process.exit();
+      }
+      
+      console.log(`Searching "${word}" now ...`);
+      return await scrape(word);
+    });
+
+    console.log(await Promise.all(asyncSearch));
+    process.stdout.write("gsearch > ");
+  });
+}
+
+main();
